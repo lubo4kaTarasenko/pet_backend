586d4d4e937acb177094f31ee11f6003a73ea08b
lubo4kaTarasenko <lubo4ka.boiko@gmail.com>
2020-11-28 12:47:20 +0200

    init pet

---
+++ lib/cat.rb
@@ -0,0 +1,60 @@
+class Cat < Pet
+    RANDOM_PHRASES = [
+      ['What the noice? Perhapse mouse. meow. i cauth you', 'you`r cat is running very fast all over the house'],
+      ['let it be sleep. murrrrr', 'your cat is falling sleep'],
+      ['ohhh what a nice new home sneakers', 'the cat sucked in you`r sneakers'] 
+    ].freeze
+  
+    def initialize(name, login)
+      super
+      @lifes = 9
+      save
+    end
+  
+    def play
+      @response << 'you`re painting  with a laser on the wall'
+      @response << 'meow , meow , i catch you. i`m a big terrible tiger'
+      super
+    end
+  
+    def voice
+      @response << 'meow, meow, mrrrr'
+      @emoji = '&#128570;'
+      lower_states
+      check
+      save
+    end
+  
+    def love
+      @response << 'your cat jump on your hands and need you pet it'
+      @response << 'murrrr murrr, i love you'
+      @emoji = '&#128571;'
+      lower_states
+      check
+      save
+    end
+  
+    def toilet
+      @response <<  'oh, you need clean the cat`s tray' if @need_toilet 
+      super
+    end
+  
+    def random
+      @response <<  RANDOM_PHRASES.sample
+      @emoji = '&#128572;'
+      lower_states
+      check
+      save
+    end
+    
+    def reset(user_login)
+      return puts("not_allowed".red) unless is_user_superadmin?(user_login)
+      self.class.new(self.name, self.user_login)
+    end
+  
+    def change_type(user_login)
+      return puts("not_allowed".red) unless is_user_admin?(user_login)
+      Dog.new(self.name, self.user_login)
+    end
+  end
+  
---
+++ lib/dog.rb
@@ -0,0 +1,55 @@
+class Dog < Pet
+
+    RANDOM_PHRASES = [
+      ['What the noice? Someone wants to kill us. Whoof whoof, you shall not pass', 'the dog barks loudly'],
+      ['i wanna you`r hugs, dude', 'your dog has come to you and wants you hug and pet it'],
+      ['ohhh what a nice new shoes', 'the dog is biting your shoes'] 
+    ].freeze
+  
+    def play
+      @response <<  'you`re throwing the ball and your dog is bringing it'
+      @response <<  'whoof whoof it is very funny'
+      super
+    end
+  
+    def voice
+      @response << 'wooof, wooof, woof'
+      @emoji = '&#128515;'
+      lower_states
+      check
+      save
+    end
+  
+    def love
+      @response << 'your dog waving his tail and need you pet it'
+      @response <<  'whoof whoof, i love you'
+      @emoji = '&#128536;'
+      lower_states
+      check
+      save
+    end
+  
+    def toilet
+      @response << 'you`re going outdors together' if @need_toilet
+      super
+    end
+  
+    def random
+      @response << RANDOM_PHRASES.sample
+      @emoji = '&#128520;'
+      lower_states
+      check
+      save
+    end
+      
+    def reset(user_login)
+      return puts("not_allowed".red) unless is_user_superadmin?(user_login)
+      self.class.new(self.name, self.user_login)
+    end
+  
+    def change_type(user_login)
+      return puts("not_allowed".red) unless is_user_admin?(user_login)
+      Dog.new(self.name)
+    end
+  end
+  
